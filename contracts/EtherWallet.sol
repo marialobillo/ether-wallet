11b991714b718dfab71061a4be5cce0a6187ac9d
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-12-03 19:34:19 +0100

    imlementing the smartcontract

--- contracts/EtherWallet.sol
+++ contracts/EtherWallet.sol
@@ -3,12 +3,26 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract EtherWallet {
+
+    address public owner;
+
+    constructor(address _owner) public {
+        owner = _owner;
+    }
     
     function deposit() payable public {
 
     }
 
     function send(address payable _to, uint _amount) public {
-        _to.transfer(_amount);
+        if(msg.sender == owner){
+            _to.transfer(_amount);
+            return;
+        }
+        revert('Sender is not allowed');
+    }
+
+    function balanceOf() view public returns(uint){
+        return address(this).balance;
     }
 }
