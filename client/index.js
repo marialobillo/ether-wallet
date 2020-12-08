a9bbcdd68f9d5c6cd65dff386bbea37853b93ec8
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-12-08 18:02:57 +0100

    send and deposit

--- client/index.js
+++ client/index.js
@@ -37,16 +37,71 @@ const initWeb3 = () => {
             .address
     );
   };
-  
+
 
   const initApp = () => {
+    const $deposit = document.getElementById('deposit');
+    const $depositResult = document.getElementById('deposit-result');
     const $send = document.getElementById('send');
-    const $getBalance = document.getElementById('getBalance');
+    const $sendResult = document.getElementById('send-result');
+    const $balance = document.getElementById('balance');
     let accounts = [];
 
     web3.eth.getAccounts()
     .then(_accounts => {
         accounts = _accounts;
     });
-  
-};
+
+    const refreshBalance = () => {
+        EtherWallet.methods
+            .balanceOf()
+            .call()
+            .then(result => {
+                $balance.innerHTML = result;
+            });
+    }
+    refreshBalance();
+
+    $deposit.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const amount = event.target.elements[0].value;
+        etherWallet.methods 
+            .deposit()
+            .send({ from: accounts[0], value: amount })
+            .then(result => {
+                $depositResult.innerHTML = `Successful ${amount} wei deposit`;
+                refreshBalance();
+            })
+            .catch(error => {
+                $depositResult.innerHTML = 'Ooops... there was an error trying to make a deposit...';
+            });
+    });
+
+    $send.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const to = event.target.elements[0].value;
+      const amount = event.target.elements[1].value;
+      etherWallet.methods
+        .send(to, amount)
+        .send({ from: accounts[0] })
+        .then(result => {
+          $sendResult.innerHTML = `Send ${amount} wei to ${to}`;
+          refreshBalance();
+        })
+        .catch(error => {
+          $sendResult.innerHTML = `Ooops...there was an error while trying to send from the contract...`;
+        });
+    });
+
+document.addEventListener('DOMContentLoaded', () => {
+  initWeb3()
+    .then(_web3 => {
+      web3 = _web3;
+      return initContract();
+    })
+    .then(_etherWallet => {
+      etherWallet = _etherWallet;
+      initApp();
+    })
+    .catch(error => console.log(error.message));
+})
